2b45acb1971344f86a9fb004de6607c8b6b7e994
Anderson Resende <andersonresende86@gmail.com>
2016-02-20 18:41:53 -0300

    Added link page

--- groups/urls.py
+++ groups/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from groups.views import ListGroups, ListLinks, GroupCreate, LinkCreate
+from groups.views import ListGroups, ListLinks, GroupCreate, LinkCreate, LinkDetail
 
 urlpatterns = [
     url(r'^groups/$', ListGroups.as_view(), name='list_groups'),
     url(r'^groups/(?P<group_id>[0-9]+)/links/$', ListLinks.as_view(), name='list_links'),
     url(r'^groups/create/$', GroupCreate.as_view(), name='create_group'),
     url(r'^groups/(?P<group_id>[0-9]+)/links/create/$', LinkCreate.as_view(), name='create_link'),
+    url(r'link/(?P<pk>\d+)/$', LinkDetail.as_view(), name='link_detail')
 ]
