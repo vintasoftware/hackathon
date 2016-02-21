deefdde2eb2fdec4fe9ce5e300c120f69f1dcd63
Hélio Meira Lins <helio@meiralins.org>
2016-02-21 15:55:18 -0300

    defining templates of the user invite email

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import url, include
 
-from authtools.views import PasswordResetConfirmAndLoginView
-
-from .views import UserLoginView, UserLogoutView, UserCreateView
+from .views import UserLoginView, UserLogoutView, UserCreateView, PasswordResetView
 
 urlpatterns = [
     url(r'^login/', UserLoginView.as_view(), name='login'),
     url(r'^logout/', UserLogoutView.as_view(), name='logout'),
     url(r'^create/', UserCreateView.as_view(), name='create'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        PasswordResetConfirmAndLoginView.as_view(), name='password_reset_confirm'),  # TODO: change this template. probably subclassing and putting a single attribute
+        PasswordResetView.as_view(), name='password_reset_confirm'),
     # url(r'^', include('authtools.urls')),
 ]
